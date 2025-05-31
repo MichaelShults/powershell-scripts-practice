a814880ae9d0b7dcd15c5f0ce6485fe623017a79
Michael Shults <mishults@gmail.com>
2025-05-31 14:02:58 +0300

    refactor backup script to use provided path instead of current directory + added current date to filename

--- folder-backup-maker.ps1
+++ folder-backup-maker.ps1
@@ -1,3 +1,9 @@
+param(
+    [Parameter(Mandatory=$True)]
+    $Path
+)
+$date = Get-Date -format "yyyy-MM-dd"
+
 if ( $null -eq $env:FBMBackupLocation) {
     $backup_folder = Read-Host "Backup folder not defined, please choose path for backups"
     [Environment]::SetEnvironmentVariable("FBMBackupLocation", $backup_folder, "User")
@@ -5,17 +11,16 @@ if ( $null -eq $env:FBMBackupLocation) {
 }
 else {
     $backup_folder = $env:FBMBackupLocation
-    $current_directory = Get-Location
-    $user_prompt = "Are you sure you want to backup folder $($current_directory)? (y/n)"
+    $user_prompt = "Are you sure you want to backup folder $Path`? (y/n)"
     $choice = Read-Host $user_prompt
     while (($choice -ne "y") -and ($choice -ne "n")) {
         Write-Host "Invalid choice."
         $choice = Read-Host $user_prompt
     }
     if ($choice -eq "y") {
-        $folder_path = Join-Path -Path (Get-Location) -ChildPath "\*"
-        $folder_name = Split-Path -Path (Get-Location) -Leaf
-        $backup_path= Join-Path -Path $backup_folder -ChildPath "$($folder_name).zip"
+        $folder_path = Join-Path -Path $Path -ChildPath "\*"
+        $folder_name = Split-Path -Path $Path -Leaf
+        $backup_path= Join-Path -Path $backup_folder -ChildPath "$($folder_name +"_" + "$date").zip"
         Compress-Archive -Path $folder_path -DestinationPAth $backup_path
         Write-Host "Backup complete at $($backup_path)."
     }
