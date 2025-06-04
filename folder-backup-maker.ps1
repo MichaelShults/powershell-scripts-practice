0021f8697c1cb852d2ec6f95709fdbe0ab6f1d8e
Michael Shults <mishults@gmail.com>
2025-06-04 22:25:40 +0300

    refactor backup script to improve user prompts and error handling + add command-based structure for backup operations

--- folder-backup-maker.ps1
+++ folder-backup-maker.ps1
@@ -1,30 +1,54 @@
 param(
-    [Parameter(Mandatory=$True)]
-    $Path
+    [Parameter(Mandatory = $true)]
+    [string]$Command,
+    [string[]]$CommandArgs
 )
-$date = Get-Date -format "yyyy-MM-dd"
-
-if ( $null -eq $env:FBMBackupLocation) {
-    $backup_folder = Read-Host "Backup folder not defined, please choose path for backups"
-    [Environment]::SetEnvironmentVariable("FBMBackupLocation", $backup_folder, "User")
-    Write-Host "Please restart terminal for change to go into effect, then run the script again."
+Function Backup {
+    param( 
+        [Parameter(Mandatory = $true)]
+        [string] $Path
+    )
+    If ($null -eq $env:FBMBackupLocation) {
+        Write-Error "Backup folder not set, please use the 'UpdateBackupPath BackupPath'"
+    }
+    If (Test-Path $Path -PathType Container) {
+        $choice = Read-Host "Confirm backup of $Path into $env:FBMBackupLocation (y/n) $"
+        If ($choice -eq "y") {
+            $date = Get-Date -format "yyyy-MM-dd"
+            $folder_path = Join-Path -Path $Path -ChildPath "\*"
+            $folder_name = Split-Path -Path $Path -Leaf
+            $backup_file_name = "$($folder_name +"_" + "$date").zip"
+            $backup_path= Join-Path -Path $env:FBMBackupLocation -ChildPath $backup_file_name
+            Compress-Archive -Path $folder_path -DestinationPAth $backup_path
+            Write-Host "Backup complete at $($backup_path)."
+        } 
+        Elseif ($choice -eq "n") {
+            Write-Host "Program finished without backup action."
+        }
+        Else {
+            Write-Error "Invalid choice."
+        }
+    } Else {
+        Write-Error "Not a valid directory."
+    }
 }
-else {
-    $backup_folder = $env:FBMBackupLocation
-    $user_prompt = "Are you sure you want to backup folder $Path`? (y/n)"
-    $choice = Read-Host $user_prompt
-    while (($choice -ne "y") -and ($choice -ne "n")) {
-        Write-Host "Invalid choice."
-        $choice = Read-Host $user_prompt
+Function UpdateBackupPath {
+    param( 
+        [Parameter(Mandatory = $true)]
+        [string] $BackupPath
+    )
+    If (Test-Path $BackupPath -PathType Container) {
+        [Environment]::SetEnvironmentVariable("FBMBackupLocation", $BackupPath, "User")
+        Write-Host "Backup folder updated, please restart terminal for changes to apply."
+    } Else {
+        Write-Error "Not a valid directory."
     }
-    if ($choice -eq "y") {
-        $folder_path = Join-Path -Path $Path -ChildPath "\*"
-        $folder_name = Split-Path -Path $Path -Leaf
-        $backup_path= Join-Path -Path $backup_folder -ChildPath "$($folder_name +"_" + "$date").zip"
-        Compress-Archive -Path $folder_path -DestinationPAth $backup_path
-        Write-Host "Backup complete at $($backup_path)."
+}
+switch ($Command) {
+    "UpdateBackupPath" {
+        UpdateBackupPath @CommandArgs
     }
-    else {
-        Write-Host "Goodbye!"
+    "Backup" {
+        Backup @CommandArgs
     }
 }
